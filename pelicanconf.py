97f57a1eb38026403a0a368956747d6d71de61d3
Lappy Lapperson <lappy@192-168-1-11.tpgi.com.au>
2020-04-27 17:23:28 +0930

    tutorial commit

---
+++ pelicanconf.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Daniel Whittenbury'
+SITENAME = "dlwhittenbury"
+SITEURL = 'https://dlwhittenbury.github.io'
+
+PATH = 'content'
+STATIC_PATHS = ['articles/AW-1-Obtaining-Loading-Querying-AW','articles/AW-2-Querying-AW-With-Python']
+# ARTICLE_PATHS = STATIC_PATHS
+
+
+TIMEZONE = 'Australia/Adelaide'
+
+DEFAULT_LANG = 'en'
+
+TYPOGRIFY = True
+
+THEME = 'voce-modified'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Home', '/index.html'),
+         ('About me', '/pages/about-me.html'),
+     	 ('Publications', '/pages/publications.html'),
+         ('Resume', '/pages/resume.html'))
+
+
+# Social Accounts
+SOCIAL = (('Email', 'mailto:whittenburydaniel@gmail.com'),
+          ('GitHub', 'https://github.com/dlwhittenbury'),
+          ('GoogleScholar', 'https://scholar.google.com/citations?user=3m-Rd7oAAAAJ&hl=en'),
+          ('ResearchGate','https://researchgate.net/profile/Daniel_Whittenbury'),
+          ('Twitter', 'https://twitter.com/dlwhittenbury'),
+          ('LinkedIn', 'https://linkedin.com/in/daniel-whittenbury-44235a155'),
+          )
+
+# ('LinkedIn', 'https://linkedin.com/in/daniel-whittenbury-44235a155'),
+# ('Inspire','http://inspirehep.net/search?p=exactauthor%3AD.L.Whittenbury.1+'),
+# ('ArXiv','https://arxiv.org/search/nucl-th?searchtype=author&query=Whittenbury%2C+D+L'),
+
+PLUGINS = ["assets"]
+PLUGIN_PATHS = ["voce-modified/plugins"]
+
+DEFAULT_PAGINATION = 6
+SUMMARY_MAX_LENGTH = 30
+
+# Publish
+DELETE_OUTPUT_DIRECTORY = False
+
+USER_LOGO_URL =  "images/myAvatar-Circle.png"
+
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
